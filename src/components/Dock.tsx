85276f5e18c62f8eaf447d7e2dbb5c4835dd4075
petsamuel <petsamuel4@gmail.com>
2025-01-06 12:49:57 +0100

    modified the orientation

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,7 +1,7 @@
 import { RiHome6Line } from "react-icons/ri";
 import { AiOutlineShop } from "react-icons/ai";
 import { FaShoppingBag, FaHeart, FaUser } from "react-icons/fa";
-import { TbMenu } from "react-icons/tb";
+import {  TbBallTennis } from "react-icons/tb";
 import { useState } from "react";
 import img from "../assets/image/6.png";
 export const FloatingNav = () => {
@@ -33,9 +33,9 @@ export const FloatingNav = () => {
     <>
       <section className="flex flex-col gap-3 items-center">
         <div className="flex lg:justify-around lg:flex-col flex-row lg:border rounded-full lg:py-4 lg:px-4 items-center gap-8 bg-[--secondary] fixed lg:relative bottom-2 justify-center w-fit place-content-center ">
-          <div className="p-6 flex justify-center items-center gap-6 lg:flex-col">
+          <div className="p-4 flex justify-center items-center gap-6 lg:flex-col">
             <div className=" rounded-full bg-[--primary] h-[3rem] w-[3rem] flex flex-col gap-3 overflow-hidden items-center ">
-              <TbMenu className="text-2xl text-white" />
+              <TbBallTennis className="text-8xl text-white" />
             </div>
             <div>
               {" "}
@@ -43,7 +43,7 @@ export const FloatingNav = () => {
                 {menu.map((val, index) => (
                   <div
                     key={index}
-                    className="flex flex-col  lg:px-4  text-2xl "
+                    className="flex flex-col  lg:px-2  text-2xl "
                     onClick={() => setActive(index)}
                   >
                     <p
@@ -72,7 +72,7 @@ export const FloatingNav = () => {
           <div className="p-3">
             <img src={img} alt="img" className="lg:w-20 lg:h-20" />
           </div>
-          <button className="lg:text-xs bg-[--secondary] text-white p-3 rounded-full text-xl w-[8rem] hover:bg-[--primary] hover:shadow-xl ">
+          <button className="lg:text-xs bg-[--secondary] text-white p-3 rounded-full text-xl w-[8rem] lg:w-fit hover:bg-[--primary] hover:shadow-xl ">
             Order Now
           </button>
         </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,21 @@
 import img4 from "../assets/image/4.png";
 import img2 from "../assets/image/2.png";
 import { TbSearch } from "react-icons/tb";
+import { TypingAnimation } from "./FlipText";
 export const Hero = () => {
   return (
     <>
       <div className="bg-[--primary] text-white lg:h-full lg:rounded-[4rem] relative w-full lg:grid  flex flex-col lg:grid-cols-5 py-10 overflow-hidden">
         <div className=" flex gap-2 col-start-4 h-[5.5rem] rounded-full -mt-[4rem] -mr-[5rem] py-2 relative ">
           <div className="relative bg-[--secondary] h-fit flex rounded-full gap-4 px-4 items-center simple-curve lg:border-[15.8px] border-[18px] border-white lg:w-[20rem] w-[25rem] justify-between z-20 after:absolute after:contents-[''] after:bg-white after:h-[5rem] after:w-[6rem] after:-end-[6rem] after:rounded-full after:-top-8 ">
-            <div className="after:absolute after:w-[3.5rem] after:h-12 after:bg-white after:contents-[''] after:-top-[2.8rem] after:-left-[4.3rem] after:rounded-full after:z-10 before:absolute before:w-[3.5rem] before:h-[59px] before:bg-[--primary] before:contents-[''] before:-left-[5.4rem] before:-top-[2.2rem] before:z-20 absolute before:rounded-full h" />
+            <div className="after:absolute after:w-[3.8rem] after:h-12 after:bg-white after:contents-[''] after:-top-[2.8rem] after:-left-[4.9rem] after:rounded-full after:z-10 before:absolute before:w-[4rem] before:h-[59px] before:bg-[--primary] before:contents-[''] before:-left-[5.8rem] before:-top-[2.2rem] before:z-20 absolute before:rounded-full" />
             <button
               type="button"
               className="flex gap-2 items-center bg-[--primary] rounded-full lg:py-3 py-2  px-8 text-lg"
             >
               <TbSearch /> <p>Search</p>
             </button>
-            <div className="p-3">
+            <div className="p-2">
               <div className="rounded-full bg-[--primary] h-[3rem] w-[3rem]" />
             </div>
           </div>
@@ -24,17 +25,23 @@ export const Hero = () => {
         </div>
 
         <div className="lg:col-start-1 lg:col-end-3 p-4 lg:row-start-2 lg:absolute scale-x-[-1]  -left-[10rem] rounded-b-3xl object-cover col-auto relative mt-[4rem] lg:mt-0">
-          <img src={img2} alt="image" className="lg:h-[92dvh] lg:inline-flex lg:relative absolute h-screen" />
+          <img
+            src={img2}
+            alt="image"
+            className="lg:inline-flex lg:relative absolute h-screen"
+          />
         </div>
         {/* content */}
         <div className="lg:col-start-3 lg:col-span-6 lg:row-start-3 lg:row-end-12 text-end lg:-mt-10 lg:z-20 relative h-full">
-          <p className="text-[7rem]  font-black uppercase text-balance lg:leading-[8rem] p-5">
-            Hit <span className="">H</span>ard
-            <br />
-            Wi<span>N</span> Big
-          </p>
-          <div className="relative lg:col-start-3 lg:col-span-6 lg:before:absolute lg:before:h-40 lg:before:w-20 lg:before:bg-white lg:before:-top-[2.5rem] lg:before:-right-[3rem] lg:after:absolute lg:after:h-[3.8rem] lg:after:w-40 lg:after:bg-[--primary]  lg:after:-top-[2.5rem] lg:after:-right-[2.5rem] lg:after:rounded-br-3xl w-full lg:mt-0  my-[6rem] overflow-hidden">
-            <div className="mt-5 p-6 border-[15.8px] lg:rounded-[5rem] bg-[--secondary] h-[18rem] lg:absolute overflow-hidden lg:w-[44rem] lg:border-white lg:z-20 -right-[3rem] w-full">
+          <TypingAnimation className="text-[7rem]  font-black uppercase text-balance p-3 lg:mt-9">
+            Hit Hard
+          </TypingAnimation>
+          <TypingAnimation className="text-[7rem]  font-black uppercase text-balance lg:mr-20 lg:mt-9">
+            Win Big
+          </TypingAnimation>
+
+          <div className="relative lg:col-start-3 lg:col-span-6 lg:before:absolute lg:before:h-[15rem] lg:before:w-20 lg:before:bg-white lg:before:-top-[2.8rem] lg:before:-right-[1rem] lg:after:absolute lg:after:h-[5.5rem] lg:after:w-[5rem] lg:after:bg-[--primary]  lg:after:-top-[4.2rem] lg:after:-right-[0rem] lg:after:rounded-br-[4.6rem] overflow-hidden lg:overflow-visible lg:mt-[3rem] bg-white">
+            <div className="mt-5 p-6 border-[15.8px] lg:rounded-[5rem] bg-[--secondary] h-[18rem] lg:absolute overflow-hidden lg:w-[44rem] border-white lg:z-20 lg:-right-[0.2rem] ">
               <div className="rounded-xl p-4 flex overflow-hidden">
                 <div className="text-start p-4 uppercase">
                   <p className="text-4xl uppercase font-bold leading-tight">
@@ -53,7 +60,7 @@ export const Hero = () => {
               </div>
             </div>
             {/*  */}
-            <div className=" relative lg:before:h-[5.8rem] lg:before:w-[18rem] lg:before:bg-white lg:before:absolute lg:before:-left-[3rem] lg:before:top-[14rem] lg:after:absolute lg:after:h-[6rem] lg:after:w-[5.3dvw] lg:after:-left-[3.9rem] lg:after:top-[13rem] lg:after:bg-[--primary] lg:after:rounded-br-full " />
+            <div className=" relative lg:before:h-[8rem] lg:before:w-[18rem] lg:before:bg-white lg:before:absolute lg:before:-left-[3.9rem] lg:before:top-[14rem] lg:after:absolute lg:after:h-[10.5dvh] lg:after:w-[8dvw] lg:after:-left-[7rem] lg:after:top-[14rem] lg:after:bg-[--primary] lg:after:rounded-br-full " />
             {/*  */}
           </div>
         </div>
